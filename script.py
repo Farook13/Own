22822c603152b78408d3abb1874c0dbd35a6ff34
Farook13 <umarulfarook787@gmail.com>
2025-03-03 23:24:54 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,12 @@
+"""
+Script templates for MovieBot.
+"""
+
+CUSTOM_FILE_CAPTION = "{file_name} - Downloaded via MovieBot"
+IMDB_TEMPLATE_TXT = """
+**Title**: {title}
+**Year**: {year}
+**Rating**: {rating}/10
+**Plot**: {plot}
+"""
+​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​#
