79b13a11c334e99b146df225894b9103b8f6d1bd
Farook13 <umarulfarook787@gmail.com>
2025-03-03 22:27:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import logging
+from pyrogram import Client
 
 logger = logging.getLogger(__name__)
 
-async def log_action(action: str):
+async def log_action(client: Client, action: str):
     logger.info(f"Action performed: {action}")
